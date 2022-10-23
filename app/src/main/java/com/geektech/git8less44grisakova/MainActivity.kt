67099723dfe842c7c894c74ab39ed6fa09540813
Kyial <Kyial4>
2022-10-23 22:03:22 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/git8less44grisakova/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.geektech.git8less44grisakova
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.geektech.git8less44grisakova.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
